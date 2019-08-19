2a01450ff8ad38014e627c15ea3d311517b51e5a
Barlamov Oleg <barlamovoleg@gmail.com>
2019-08-19 23:39:28 +0500

    references to projects. start hexo lib

--- Libs/HexoGrid/HexPointExtensions.cs
+++ Libs/HexoGrid/HexPointExtensions.cs
@@ -5,10 +5,25 @@ namespace HexoGrid
 {
     public static class HexPointExtensions
     {
-        [NotNull, ItemNotNull]
+		[NotNull, ItemNotNull]
         public static IEnumerable<HexPoint> GetAroundPoints(this HexPoint point, bool includeSelf = false)
-        {
+		{
+			var parity = point.Y;
+			var dir = Directions[parity]
+		}
 
-        }
-    }
+	    private static readonly HexPoint[][] Directions = 
+		{
+		    new[]
+		    {
+			    new HexPoint(1,0), new HexPoint(0,-1), new HexPoint(-1,-1),
+				new HexPoint(-1,0), new HexPoint(-1,1), new HexPoint(0,1),
+			},
+		    new[]
+		    {
+			    new HexPoint(1,0), new HexPoint(1,-1), new HexPoint(0,-1),
+			    new HexPoint(-1,0), new HexPoint(0,1), new HexPoint(1,1),
+		    }
+		};
+	}
 }
