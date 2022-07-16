87dbf8eb95ce3d0f0b912f3e08903ba1ccc886ea
Oleg Barlamov <oleg.b@miro.com>
2022-07-16 13:24:51 +0200

    missed file for the previous commit

--- Games.X4/Atom.Client/ScenesComponents/WorldMapCellContent/WorldMapCellContentController.cs
+++ Games.X4/Atom.Client/ScenesComponents/WorldMapCellContent/WorldMapCellContentController.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Threading;
-using FrameworkSDK.Common;
+using Atom.Client.Services;
 using FrameworkSDK.MonoGame.Core;
-using FrameworkSDK.MonoGame.Graphics.Camera3D;
 using FrameworkSDK.MonoGame.Mvc;
 using FrameworkSDK.MonoGame.Resources.Generation;
 using FrameworkSDK.MonoGame.Services;
 using JetBrains.Annotations;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using X4World.Maps;
 using X4World.Objects;
 
@@ -15,24 +16,24 @@ namespace Atom.Client.Components
     public class WorldMapCellContentController : Controller<WorldMapCellContent>
     {
         [NotNull] private IBackgroundTasksProcessor BackgroundTasksProcessor { get; }
-        [NotNull] private ICamera3DProvider Camera3DProvider { get; }
+        [NotNull] private IPlayerProvider PlayerProvider { get; }
         [NotNull] private ITextureGeneratorService TextureGeneratorService { get; }
-        private IRandomService RandomService { get; }
-
+        
         private CancellationTokenSource _textureGenerationCancellationTokenSource;
+        
+        private readonly object _locker = new object();
 
         public WorldMapCellContentController(
             [NotNull] WorldMapCellContent model,
             [NotNull] IBackgroundTasksProcessor backgroundTasksProcessor,
-            [NotNull] ICamera3DProvider camera3DProvider,
-            [NotNull] ITextureGeneratorService textureGeneratorService,
-            [NotNull] IRandomService randomService)
+            [NotNull] IPlayerProvider playerProvider,
+            [NotNull] ITextureGeneratorService textureGeneratorService)
         {
             if (model == null) throw new ArgumentNullException(nameof(model));
             BackgroundTasksProcessor = backgroundTasksProcessor ?? throw new ArgumentNullException(nameof(backgroundTasksProcessor));
-            Camera3DProvider = camera3DProvider ?? throw new ArgumentNullException(nameof(camera3DProvider));
+            PlayerProvider = playerProvider ?? throw new ArgumentNullException(nameof(playerProvider));
             TextureGeneratorService = textureGeneratorService ?? throw new ArgumentNullException(nameof(textureGeneratorService));
-            RandomService = randomService ?? throw new ArgumentNullException(nameof(randomService));
+
             SetModel(model);
         }
 
@@ -40,39 +41,79 @@ namespace Atom.Client.Components
         {
             base.OnAttached(scene);
             
-            var cameraPosition = Camera3DProvider.GetActiveCamera().GetPosition();
-            var mapPoint = GlobalWorldMap.MapPointFromWorld(cameraPosition);
-            if (!DataModel.WorldMapCellAggregatedData.WorldMapCellTextureData.IsTextureExist ||
-                mapPoint != DataModel.WorldMapCellAggregatedData.WorldMapCellTextureData.TextureTargetPoint)
+            var cameraPosition = PlayerProvider.GetPlayerPosition();
+            DataModel.WorldMapCellAggregatedData.WorldMapCellTextureData.Rotation =
+                GetBillboardRotation(DataModel.GetWorldPosition(), cameraPosition, Vector3.Up);
+            
+            using (_textureGenerationCancellationTokenSource)
             {
-                using (_textureGenerationCancellationTokenSource)
-                {
-                    _textureGenerationCancellationTokenSource?.Cancel(); 
-                }
-                _textureGenerationCancellationTokenSource = new CancellationTokenSource();
-
-                BackgroundTasksProcessor.EnqueueTask(new SimpleDelayedTask((time, token) =>
-                {
-                    try
-                    {
-                        var texture = MapCellTextureGenerator.GenerateAsync(
-                            DataModel.GetWorldPosition(),
-                            GlobalWorldMap.WorldFromMapPoint(mapPoint),
-                            DataModel.WorldMapCellAggregatedData.GalaxiesPoints,
-                            TextureGeneratorService, RandomService, token);
-                        
-                        token.ThrowIfCancellationRequested();
-
-                        var oldTexture = DataModel.WorldMapCellAggregatedData.WorldMapCellTextureData.Texture; 
-                        DataModel.WorldMapCellAggregatedData.WorldMapCellTextureData.AssignNewTexture(mapPoint, texture);
-                        oldTexture?.Dispose();
-                    }
-                    catch (OperationCanceledException)
-                    {
-                        //ignored
-                    }
-                }, _textureGenerationCancellationTokenSource.Token));
+                _textureGenerationCancellationTokenSource?.Cancel();
             }
+
+            _textureGenerationCancellationTokenSource = new CancellationTokenSource();
+            
+            BackgroundTasksProcessor.EnqueueTask(new SimpleDelayedTask((time, token) =>
+            {
+                try
+                {
+                    var mapPoint = GlobalWorldMap.MapPointFromWorld(cameraPosition);
+                    
+                    var texture = MapCellTextureGenerator.GenerateAsync(
+                        DataModel.GetWorldPosition(),
+                        GlobalWorldMap.WorldFromMapPoint(mapPoint),
+                        DataModel.WorldMapCellAggregatedData.GalaxiesPoints,
+                        TextureGeneratorService, token);
+
+                    token.ThrowIfCancellationRequested();
+
+                    Texture2D oldTexture;
+                    lock (_locker)
+                    {
+                        oldTexture = DataModel.WorldMapCellAggregatedData.WorldMapCellTextureData.Texture;
+                        DataModel.WorldMapCellAggregatedData.WorldMapCellTextureData
+                            .AssignNewTexture(mapPoint, texture);
+                    }
+                    oldTexture?.Dispose();
+                }
+                catch (OperationCanceledException)
+                {
+                    //ignored
+                }
+            }, _textureGenerationCancellationTokenSource.Token));
+        }
+
+        private Matrix GetBillboardRotation(Vector3 objectPosition, Vector3 cameraPosition, Vector3 cameraUp)
+        {
+            var normal = cameraPosition - objectPosition;
+            normal.Normalize();
+            if (normal == Vector3.Up)
+                return Matrix.Identity;
+            if (normal == Vector3.Down)
+                return Matrix.CreateRotationX(MathHelper.Pi);
+            
+            var rotationAxis = Vector3.Cross(cameraUp, normal);
+            rotationAxis.Normalize();
+            var down = Vector3.Cross(normal, rotationAxis);
+            return Matrix.CreateRotationX(3 * MathHelper.Pi / 2) * new Matrix
+            {
+                // Billboard with inverted M11-M12-M13
+                M11 = -rotationAxis.X,
+                M12 = -rotationAxis.Y,
+                M13 = -rotationAxis.Z,
+                M14 = 0.0f,
+                M21 = down.X,
+                M22 = down.Y,
+                M23 = down.Z,
+                M24 = 0.0f,
+                M31 = normal.X,
+                M32 = normal.Y,
+                M33 = normal.Z,
+                M34 = 0.0f,
+                M41 = 0f,
+                M42 = 0f,
+                M43 = 0f,
+                M44 = 1f
+            };
         }
 
         protected override void OnDetached(SceneBase scene)
